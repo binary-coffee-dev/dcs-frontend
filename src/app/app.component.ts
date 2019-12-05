bbf912c949e76e2af77d72e0818e0bbbbd6e9c34
Guillermo Gonzalez <ggjnez92@gmail.com>
2019-12-06 00:55:15 +0100

    Add example of get posts list in the app component (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const POSTS_QUERY = gql` { posts { title body } } `;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dcs-frontend';
+
+  constructor(private apollo: Apollo) {
+  }
+
+  ngOnInit(): void {
+    this.apollo
+      .watchQuery({query: POSTS_QUERY})
+      .valueChanges.subscribe(result => {
+      console.log(result);
+    });
+  }
+
 }
