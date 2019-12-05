ff0b6ccd7fa1d4cf7e272c1aec58351cc9546c37
Guillermo Gonzalez <ggjnez92@gmail.com>
2019-12-06 00:54:47 +0100

    Add graphql url environment variable (#2)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ import sharedEnvironment from './base';
 export const environment = {
   ...sharedEnvironment,
   apiUrl: sharedEnvironment['apiUrl'] || 'http://localhost:1337/',
+  graphqlUrl: sharedEnvironment['graphqlUrl'] || 'http://localhost:1337/graphql',
   production: sharedEnvironment['production'] || false
 };
