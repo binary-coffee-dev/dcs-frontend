075cbc12c9d8ee617765454280960e9e1d19dfb8
Guillermo Gonzalez <ggjnez92@gmail.com>
2019-12-10 01:08:06 +0100

    Increase coverage in post state (#19)

--- src/app/core/redux/states/post.state.spec.ts
+++ src/app/core/redux/states/post.state.spec.ts
@@ -1,12 +1,14 @@
 import {async, TestBed} from '@angular/core/testing';
 
-import {NgxsModule, Store} from '@ngxs/store';
+import {NgxsModule, StateContext, Store} from '@ngxs/store';
 import {of} from 'rxjs';
 
 import {PostState} from './post.state';
 import {PostService} from '../services';
-import {FetchPostAction, FetchPostsAction} from '../actions';
-import {Post} from '../models';
+import {FetchPostAction, FetchPostsAction, NextPageAction, PreviousPageAction} from '../actions';
+import {Post, PostConnection} from '../models';
+import {PostStateModel} from './post-state.model';
+import {combineAll} from 'rxjs/operators';
 
 class PostServiceStub {
   fetchPosts = jest.fn();
@@ -15,6 +17,10 @@ class PostServiceStub {
 
 const ID_EXAMPLE = '123';
 const POST_EXAMPLE = {id: ID_EXAMPLE} as Post;
+const PAGE_SIZE_EXAMPLE = 10;
+const PAGE_EXAMPLE = 1;
+const COUNT_EXAMPLE = 1;
+const START_PAGE_EXAMPLE = 0;
 const POSTS_EXAMPLE = [
   POST_EXAMPLE,
   {id: '12'} as Post
@@ -41,11 +47,26 @@ describe('PostState', () => {
     expect(postState).toBeTruthy();
   });
 
-  xit('should fetch the list of posts', (done) => {
-    jest.spyOn(postService, 'fetchPosts').mockReturnValue(of(POSTS_EXAMPLE));
-    store.dispatch(new FetchPostsAction());
-    store.selectOnce(PostState.posts).subscribe((posts) => {
-      expect(posts).toEqual(POSTS_EXAMPLE);
+  it('should fetch the list of posts', (done) => {
+    jest.spyOn(postState, 'fetchPage').mockReturnValue(of({} as PostConnection));
+    store.dispatch(new FetchPostsAction()).subscribe(() => {
+      expect(postState.fetchPage).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('should fetch next list of post', (done) => {
+    jest.spyOn(postState, 'fetchPage').mockReturnValue(of({} as PostConnection));
+    store.dispatch(new NextPageAction()).subscribe(() => {
+      expect(postState.fetchPage).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('should fetch previous list of post', (done) => {
+    jest.spyOn(postState, 'fetchPage').mockReturnValue(of({} as PostConnection));
+    store.dispatch(new PreviousPageAction()).subscribe(() => {
+      expect(postState.fetchPage).toHaveBeenCalled();
       done();
     });
   });
@@ -71,5 +92,39 @@ describe('PostState', () => {
     expect(postState.nextPageNumber(0, 60, 20)).toEqual(1);
     expect(postState.nextPageNumber(1, 60, 20)).toEqual(2);
     expect(postState.nextPageNumber(2, 60, 20)).toEqual(2);
+  });
+
+  it('should set firstPage=true and lastPage=false', () => {
+    const ctx = {patchState: jest.fn()} as unknown as StateContext<PostStateModel>;
+    postState.refreshPaginationVisibility(ctx, 0, 2, 1);
+    expect(ctx.patchState).toHaveBeenCalledWith({firstPage: true, lastPage: false});
+  });
+
+  it('should set firstPage=false and lastPage=true', () => {
+    const ctx = {patchState: jest.fn()} as unknown as StateContext<PostStateModel>;
+    postState.refreshPaginationVisibility(ctx, 1, 2, 1);
+    expect(ctx.patchState).toHaveBeenCalledWith({firstPage: false, lastPage: true});
+  });
+
+  it('should set firstPage=true and lastPage=true', () => {
+    const ctx = {patchState: jest.fn()} as unknown as StateContext<PostStateModel>;
+    postState.refreshPaginationVisibility(ctx, 0, 1, 1);
+    expect(ctx.patchState).toHaveBeenCalledWith({firstPage: true, lastPage: true});
+  });
+
+  it('should fetch list of posts', (done) => {
+    jest.spyOn(postService, 'fetchPosts').mockReturnValue(of({
+      values: POSTS_EXAMPLE,
+      aggregate: {count: COUNT_EXAMPLE}
+    } as PostConnection));
+    jest.spyOn(postState, 'refreshPaginationVisibility').mockImplementation(jest.fn());
+    const ctx = {patchState: jest.fn()} as unknown as StateContext<PostStateModel>;
+
+    postState.fetchPage(PAGE_SIZE_EXAMPLE, START_PAGE_EXAMPLE, ctx, PAGE_EXAMPLE).subscribe(() => {
+      expect(postService.fetchPosts).toHaveBeenCalledWith(PAGE_SIZE_EXAMPLE, START_PAGE_EXAMPLE);
+      expect(ctx.patchState).toHaveBeenCalledWith({posts: POSTS_EXAMPLE, count: COUNT_EXAMPLE});
+      expect(postState.refreshPaginationVisibility).toHaveBeenCalledWith(ctx, PAGE_EXAMPLE, COUNT_EXAMPLE, PAGE_SIZE_EXAMPLE);
+      done();
+    });
   });
 });
