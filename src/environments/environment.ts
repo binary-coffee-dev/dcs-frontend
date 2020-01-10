1c0ba993b92fa4f9701952a328e0597279ce0845
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-10 21:40:58 +0100

    Add siteUrl variable to environment files (#33)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
   ...sharedEnvironment,
   apiUrl: sharedEnvironment['apiUrl'] || 'http://localhost:1337/',
   graphqlUrl: sharedEnvironment['graphqlUrl'] || 'http://localhost:1337/graphql',
+  siteUrl: sharedEnvironment['siteUrl'] || 'https://binary-coffee.dev',
   production: sharedEnvironment['production'] || false
 };
