60a10e0b382c5f26993260f165421e9f4a4adf54
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-12 01:56:27 +0100

    Add publishedAt attribute to post graphql queries

--- src/app/core/graphql/queries/posts.query.ts
+++ src/app/core/graphql/queries/posts.query.ts
@@ -2,14 +2,14 @@ import gql from 'graphql-tag';
 
 export const POSTS_QUERY = gql`
     query pageQuery($limit: Int!, $start: Int!) {
-        postsConnection(sort: "createdAt:desc", limit: $limit, start: $start){
+        postsConnection(sort: "publishedAt:desc", limit: $limit, start: $start){
             values {
                 id
                 name
                 title
                 body
                 description
-                createdAt
+                publishedAt
                 views
                 banner {
                     name
