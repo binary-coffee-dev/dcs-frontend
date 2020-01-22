0133540d3cf3b372fb71ca8adddddd2e073cda73
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-01-22 23:05:03 +0100

    Create comments styles and visual elements (#41)

--- src/app/features/post/comments/comments.component.ts
+++ src/app/features/post/comments/comments.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.css']
+  styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent implements OnInit {
 
