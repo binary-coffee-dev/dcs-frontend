6d3d0f3d1b85047e1c32bae17e868dc582374197
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-01-26 11:38:33 -0500

    Header, Footer y Social-links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './features/app.component';
-import {ReduxModule} from './core/redux';
-import {MaterialModule} from './core/material';
-import {HeaderComponent} from './features/components/header';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './features/app.component';
+import { ReduxModule } from './core/redux';
+import { MaterialModule } from './core/material';
+import { HeaderComponent } from './features/components/header';
+import { FooterComponent } from './features/components/footer/footer.component';
+import { SocialLinksComponent } from './features/components/social-links/social-links.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SocialLinksComponent
+  ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
