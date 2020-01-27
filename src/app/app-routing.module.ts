efb68d9273992dc6eb9446e634e168e9d778df94
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-01-26 22:46:44 -0500

    Add component info/about as router about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from "./features/info/contact/contact.component";
+import { AboutComponent } from "./features/info/about/about.component";
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'contact',
     component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
--- src/app/features/info/info.module.ts
+++ src/app/features/info/info.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
-  declarations: [ContactComponent],
+  declarations: [ContactComponent, AboutComponent],
   imports: [
     CommonModule
   ]
