534a3b99300841c79496cffb85cc15dd3cd103eb
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-02-05 12:41:32 -0500

    Scroll top UI

--- src/app/features/components/scroll-top/scroll-top.component.ts
+++ src/app/features/components/scroll-top/scroll-top.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
 @Component({
   selector: 'app-scroll-top',
   templateUrl: './scroll-top.component.html',
-  styleUrls: ['./scroll-top.component.css']
+  styleUrls: ['./scroll-top.component.scss', '../social-links/social-links.component.scss']
 })
 export class ScrollTopComponent implements OnInit {
-
+  windowScrolled: boolean;
   constructor() { }
-
-  ngOnInit() {
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+    }
+    else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = false;
+    }
   }
-
+  scrollToTop() {
+    (function smoothscroll() {
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 8));
+      }
+    })();
+  }
+  ngOnInit() { }
 }
