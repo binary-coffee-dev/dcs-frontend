452485abd560c5e2d04f9e1578e0203d3b8357ab
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-02-05 14:34:11 -0500

    Fixing reviews

--- src/app/features/components/scroll-top/scroll-top.component.ts
+++ src/app/features/components/scroll-top/scroll-top.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-scroll-top',
   templateUrl: './scroll-top.component.html',
   styleUrls: ['./scroll-top.component.scss', '../social-links/social-links.component.scss']
 })
-export class ScrollTopComponent implements OnInit {
+export class ScrollTopComponent {
   windowScrolled: boolean;
   constructor() { }
   @HostListener("window:scroll", [])
@@ -25,5 +25,4 @@ export class ScrollTopComponent implements OnInit {
       }
     })();
   }
-  ngOnInit() { }
 }
