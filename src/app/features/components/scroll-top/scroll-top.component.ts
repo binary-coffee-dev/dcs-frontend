e7bf7cf8f70a3f73659382ce22dc0bc256bede1d
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-02-05 12:03:23 -0500

    Scroll top component created

---
+++ src/app/features/components/scroll-top/scroll-top.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-top',
+  templateUrl: './scroll-top.component.html',
+  styleUrls: ['./scroll-top.component.css']
+})
+export class ScrollTopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
