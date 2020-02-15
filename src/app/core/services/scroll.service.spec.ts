619df781fec546a7727e4843f668e76d520ad20a
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-02-15 13:50:53 +0100

    Fix broken tests (#94)

--- src/app/core/services/scroll.service.spec.ts
+++ src/app/core/services/scroll.service.spec.ts
@@ -1,9 +1,14 @@
 import {TestBed} from '@angular/core/testing';
 
 import {ScrollService} from './scroll.service';
+import {WINDOW} from './configs';
+
+const window = {};
 
 describe('ScrollService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [{provide: WINDOW, useValue: window}]
+  }));
 
   it('should be created', () => {
     const service: ScrollService = TestBed.get(ScrollService);
