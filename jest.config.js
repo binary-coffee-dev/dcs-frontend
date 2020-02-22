d3cbd3b44c6d394a659373d6fefb2e9a5555de2e
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-02-22 23:49:20 +0100

    Update admin project deployment (#95)

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageReporters: ['html', 'lcov'],
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ['./test-setup.ts']
 };
