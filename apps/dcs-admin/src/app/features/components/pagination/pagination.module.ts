80efb161241cba8e8d0f0fedd05851e333bc0f7f
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-02-22 19:56:44 +0100

    Fix styles in the tables and the pagination (#95)

--- apps/dcs-admin/src/app/features/components/pagination/pagination.module.ts
+++ apps/dcs-admin/src/app/features/components/pagination/pagination.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
+import {MaterialModule} from '@dcs-libs/shared';
 import {PaginationComponent} from './pagination.component';
 
 @NgModule({
@@ -9,7 +10,8 @@ import {PaginationComponent} from './pagination.component';
     PaginationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class PaginationModule {
