6ff2a53d5a788de0d4d3f291aafa6a461ec1329f
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-02-23 22:52:52 +0100

    Remove empty space in console message

--- apps/dcs-frontend/src/app/features/console.log.ts
+++ apps/dcs-frontend/src/app/features/console.log.ts
@@ -33,5 +33,5 @@ export const consoleMessage = `
 Welcome to Binary Coffee console, if you see any bug or issue, do not hesitate to inform us. Remember that our code is completely open-source so visit our repo if you want to contribute.
 https://github.com/dcs-community
 
- 
+
 `;
