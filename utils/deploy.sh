0e4c5a747f7146b7f850de627866fe4ed6abd022
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-02-23 22:58:50 +0100

    Fix issue in the script (#116)

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 BASE_BRANCH=$(git log --first-parent --format=format:'%H' | head -2 | tail -1)
-BASE_BRANCH="e5b60005"
+#BASE_BRANCH="e5b60005"
 HEAD_BRANCH="HEAD"
 
 AFFECTED_APPS=$(nx affected:apps --base=$BASE_BRANCH --head=$HEAD_BRANCH --plain)
