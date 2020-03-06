6eafa07bb48f1e51295c49e1b2a9d88f89027269
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-03-06 13:51:16 +0100

    Update environments files in blog frontend (#163)

--- apps/dcs-frontend/src/environments/environment.dev.ts
+++ apps/dcs-frontend/src/environments/environment.dev.ts
@@ -1,7 +1,9 @@
+import {Environment} from '@dcs-libs/shared';
+
 export const environment = {
   apiUrl: 'https://api-dev.binary-coffee.dev/',
   graphqlUrl: 'https://api-dev.binary-coffee.dev/graphql',
   siteUrl: 'https://dev.binary-coffee.dev',
   googleAnalyticsId: 'UA-157555549-2',
   production: false
-};
+} as Environment;
--- apps/dcs-frontend/src/environments/environment.prod.ts
+++ apps/dcs-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
+import {Environment} from '@dcs-libs/shared';
+
 export const environment = {
   apiUrl: 'https://api.binary-coffee.dev/',
   graphqlUrl: 'https://api.binary-coffee.dev/graphql',
   siteUrl: 'https://binary-coffee.dev',
   googleAnalyticsId: 'UA-157555549-1',
   production: true
-};
+} as Environment;
