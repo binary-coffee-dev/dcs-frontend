cb299d45ca08ebba881a6194c5cb34b7d9d719a9
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-03-27 18:08:59 -0400

    Add Tags component

---
+++ libs/shared/src/lib/components/tags/tags.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+import { Tag } from '@dcs-libs/shared';
+
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.scss']
+})
+export class TagsComponent {
+
+  @Input()
+  tags: Tag[];
+
+  constructor() { }
+}
