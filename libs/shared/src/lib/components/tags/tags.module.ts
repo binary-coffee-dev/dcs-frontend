91dd29f79fbd6d2d3819c657dde6070a6b8168ff
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-03-27 18:09:11 -0400

    Tags module

---
+++ libs/shared/src/lib/components/tags/tags.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+
+import { TagsComponent } from "./tags.component";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [TagsComponent],
+  exports: [TagsComponent]
+})
+export class TagsModule { }
