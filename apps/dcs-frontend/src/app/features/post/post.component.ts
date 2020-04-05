4dc2745bac7976d2d73e3873fe80c0d606852134
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-04-05 00:58:23 -0400

    Edit post redirection

--- apps/dcs-frontend/src/app/features/post/post.component.ts
+++ apps/dcs-frontend/src/app/features/post/post.component.ts
@@ -1,17 +1,26 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {Title} from '@angular/platform-browser';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
-import {Store} from '@ngxs/store';
+import { Store } from '@ngxs/store';
 
-import {Environment, ENVIRONMENT, FetchCommentsAction, Post, PostState} from '@dcs-libs/shared';
-import {MetaTag, MetaTagsService, MomentService, ResourceService, ScrollService} from '../../core/services';
+import {
+  ENVIRONMENT,
+  Environment,
+  FetchCommentsAction,
+  Post,
+  PostState,
+  AuthState,
+  Permissions,
+  WINDOW
+} from '@dcs-libs/shared';
+import { MetaTag, MetaTagsService, MomentService, ResourceService, ScrollService } from '../../core/services';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit {
+export class PostComponent extends Permissions implements OnInit {
 
   post: Post;
 
@@ -22,8 +31,10 @@ export class PostComponent implements OnInit {
     private metaTags: MetaTagsService,
     private title: Title,
     private scroll: ScrollService,
+    @Inject(WINDOW) private window: Window,
     @Inject(ENVIRONMENT) private environment: Environment
   ) {
+    super();
   }
 
   ngOnInit() {
@@ -33,13 +44,13 @@ export class PostComponent implements OnInit {
         this.post = post;
         const imageUrl = post.banner ? new URL(post.banner.url, this.environment.apiUrl).toString() : '';
         this.metaTags.updateMetas([
-          {key: MetaTagsService.metas, value: new URL(`post/${post.name}`, this.environment.siteUrl).toString()} as MetaTag,
-          {key: MetaTagsService.titleMeta, value: post.title} as MetaTag,
-          {key: MetaTagsService.imageMeta, value: imageUrl} as MetaTag,
-          {key: MetaTagsService.descriptionMeta, value: post.description} as MetaTag,
-          {key: MetaTagsService.twitterImageMeta, value: imageUrl} as MetaTag,
-          {key: MetaTagsService.typeMeta, value: 'article'} as MetaTag,
-          {key: MetaTagsService.twitterTitleMeta, value: post.title} as MetaTag
+          { key: MetaTagsService.metas, value: new URL(`post/${post.name}`, this.environment.siteUrl).toString() } as MetaTag,
+          { key: MetaTagsService.titleMeta, value: post.title } as MetaTag,
+          { key: MetaTagsService.imageMeta, value: imageUrl } as MetaTag,
+          { key: MetaTagsService.descriptionMeta, value: post.description } as MetaTag,
+          { key: MetaTagsService.twitterImageMeta, value: imageUrl } as MetaTag,
+          { key: MetaTagsService.typeMeta, value: 'article' } as MetaTag,
+          { key: MetaTagsService.twitterTitleMeta, value: post.title } as MetaTag
         ]);
         this.title.setTitle(post.title);
 
@@ -48,4 +59,12 @@ export class PostComponent implements OnInit {
     });
   }
 
+  isMyPost(post) {
+    const user = this.store.selectSnapshot(AuthState.me);
+    return !!user && !!post.author && post.author.id === user.id;
+  }
+
+  editPost(post) {
+    this.window.location.href = `${this.environment.siteDashboardUrl}/articles/update/${post.id}`;
+  }
 }
