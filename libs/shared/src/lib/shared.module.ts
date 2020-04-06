d47b88d9e969d3a39101b3950bed5543eb3be755
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-04-06 10:44:18 -0400

    Fixing lint

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './features/loading/loading.component';
 import { WINDOW, windowFactory } from './core/models';
 import { HasPermissionsPipe } from './core/pipes/has-permissions.pipe';
-import { HasPermissionsPipeStub } from "./core/pipes/has-permissions.pipe.stub";
+import { HasPermissionsPipeStub } from './core/pipes/has-permissions.pipe.stub';
 import { ROLE_PERMISSION_MAP, rolePermissionMap } from './core/permissions';
 
 @NgModule({
