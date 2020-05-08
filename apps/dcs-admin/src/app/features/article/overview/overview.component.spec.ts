17ada7972811213e66306a21b71f1a0fded00310
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-05-08 12:02:00 +0200

    Fix broken test (#235)

--- apps/dcs-admin/src/app/features/article/overview/overview.component.spec.ts
+++ apps/dcs-admin/src/app/features/article/overview/overview.component.spec.ts
@@ -2,13 +2,14 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {RouterTestingModule} from '@angular/router/testing';
 import {MatDialog} from '@angular/material/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {of} from 'rxjs';
 import {Store} from '@ngxs/store';
+import {TagInputModule} from 'ngx-chips';
 
 import {HasPermissionsPipeStub, MaterialModule, UrlUtilsService} from '@dcs-libs/shared';
 import {OverviewComponent} from './overview.component';
-import {of} from 'rxjs';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 class StoreStub {
   dispatch = jest.fn();
@@ -34,7 +35,7 @@ describe('OverviewComponent', () => {
         {provide: MatDialog, useClass: MatDialogStub},
         {provide: UrlUtilsService, useClass: UrlUtilsServiceStub}
       ],
-      imports: [RouterTestingModule, MaterialModule, BrowserAnimationsModule],
+      imports: [RouterTestingModule, MaterialModule, BrowserAnimationsModule, TagInputModule],
       schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
