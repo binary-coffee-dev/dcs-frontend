c10ef1ca21f52ee0ed3160d3283fb59a40680fc9
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-05-14 14:32:37 +0200

    Create new query to get list of similar posts by post id

---
+++ libs/shared/src/lib/core/graphql/queries/posts-similar.ts
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+
+export const SIMILAR_POSTS_QUERY = gql`
+    query similarPosts($id: ID!, $limit: Int){
+      similarPosts(id: $id, limit: $limit){
+        title
+        description
+        banner {
+          url
+        }
+        publishedAt
+        id
+      }
+    }
+`;
