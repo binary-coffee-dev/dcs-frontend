be4ef1df309d55bbf046880f71de328c01e5582f
Raul C. Rivero <rahulrcr98@gmail.com>
2020-05-25 13:37:34 -0400

    Change meta title and description of posts (#276)

--- apps/dcs-frontend/src/app/features/post/post.component.ts
+++ apps/dcs-frontend/src/app/features/post/post.component.ts
@@ -45,14 +45,14 @@ export class PostComponent extends Permissions implements OnInit {
         const imageUrl = post.banner ? new URL(post.banner.url, this.environment.apiUrl).toString() : '';
         this.metaTags.updateMetas([
           { key: MetaTagsService.metas, value: new URL(`post/${post.name}`, this.environment.siteUrl).toString() } as MetaTag,
-          { key: MetaTagsService.titleMeta, value: post.title } as MetaTag,
+          { key: MetaTagsService.titleMeta, value: `🥇 | ${post.title}` } as MetaTag,
           { key: MetaTagsService.imageMeta, value: imageUrl } as MetaTag,
-          { key: MetaTagsService.descriptionMeta, value: post.description } as MetaTag,
+          { key: MetaTagsService.descriptionMeta, value: `✅ ${post.description}` } as MetaTag,
           { key: MetaTagsService.twitterImageMeta, value: imageUrl } as MetaTag,
           { key: MetaTagsService.typeMeta, value: 'article' } as MetaTag,
           { key: MetaTagsService.twitterTitleMeta, value: post.title } as MetaTag
         ]);
-        this.title.setTitle(post.title);
+        this.title.setTitle(`🥇 | ${post.title}`);
 
         this.store.dispatch(new FetchCommentsAction(post.id));
       }
