e777310f53df5549d5093418316c33c64ea45fa7
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-05-27 22:44:55 +0200

    Update the position of the icon in the meta title

--- apps/dcs-frontend/src/app/features/post/post.component.ts
+++ apps/dcs-frontend/src/app/features/post/post.component.ts
@@ -46,7 +46,7 @@ export class PostComponent extends Permissions implements OnInit {
         const imageUrl = post.banner ? new URL(post.banner.url, this.environment.apiUrl).toString() : '';
         this.metaTags.updateMetas([
           { key: MetaTagsService.metas, value: new URL(`post/${post.name}`, this.environment.siteUrl).toString() } as MetaTag,
-          { key: MetaTagsService.titleMeta, value: `🥇 | ${post.title}` } as MetaTag,
+          { key: MetaTagsService.titleMeta, value: `${post.title} | 🥇` } as MetaTag,
           { key: MetaTagsService.imageMeta, value: imageUrl } as MetaTag,
           { key: MetaTagsService.descriptionMeta, value: `✅ ${post.description}` } as MetaTag,
           { key: MetaTagsService.twitterImageMeta, value: imageUrl } as MetaTag,
