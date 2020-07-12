5a63d83c47f381e5f99df85ad429143f86da5609
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-07-12 18:48:42 +0200

    Fix broken tests (#144)

--- apps/dcs-frontend/src/app/features/dashboard/filter/filter.component.spec.ts
+++ apps/dcs-frontend/src/app/features/dashboard/filter/filter.component.spec.ts
@@ -1,6 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {RouterTestingModule} from '@angular/router/testing';
 
-import { FilterComponent } from './filter.component';
+import {Store} from '@ngxs/store';
+
+import {FilterComponent} from './filter.component';
+
+class StoreStub {
+}
 
 describe('FilterComponent', () => {
   let component: FilterComponent;
@@ -8,14 +15,19 @@ describe('FilterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FilterComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [FilterComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{provide: Store, useClass: StoreStub}]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FilterComponent);
     component = fixture.componentInstance;
+
+    spyOn(component, 'ngOnInit');
+
     fixture.detectChanges();
   });
 
