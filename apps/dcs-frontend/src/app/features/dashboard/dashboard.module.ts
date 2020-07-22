48dd21e8c4fef072336c8f44de345e417fe3b712
Karel Diaz <kdiazalfonso94@gmail.com>
2020-07-21 22:03:59 -0400

    Add recent podcast components

--- apps/dcs-frontend/src/app/features/dashboard/dashboard.module.ts
+++ apps/dcs-frontend/src/app/features/dashboard/dashboard.module.ts
@@ -14,6 +14,7 @@ import {PaginationComponent} from './pagination';
 import {RecentCommentsComponent} from './recent-comments/recent-comments.component';
 import {LimitTextPipe} from './recent-comments/limit-text.pipe';
 import { FilterComponent } from './filter/filter.component';
+import { RecentPodcastsComponent } from './recent-podcasts/recent-podcasts.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FilterComponent } from './filter/filter.component';
     PaginationComponent,
     RecentCommentsComponent,
     LimitTextPipe,
-    FilterComponent
+    FilterComponent,
+    RecentPodcastsComponent
   ],
   imports: [
     CommonModule,
