7f8a91e85d666be99f46014e6d3200c06c8c8b27
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-08-16 16:54:52 -0400

    Fixing test

--- apps/dcs-frontend/src/app/features/podcast/espacio-binario/espacio-binario.component.spec.ts
+++ apps/dcs-frontend/src/app/features/podcast/espacio-binario/espacio-binario.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { EspacioBinarioComponent } from './espacio-binario.component';
 
@@ -8,7 +9,8 @@ describe('EspacioBinarioComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EspacioBinarioComponent ]
+      declarations: [ EspacioBinarioComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
--- apps/dcs-frontend/src/app/features/podcast/podcast.component.spec.ts
+++ apps/dcs-frontend/src/app/features/podcast/podcast.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { PodcastComponent } from './podcast.component';
 
@@ -8,7 +9,8 @@ describe('PodcastComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PodcastComponent ]
+      declarations: [ PodcastComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
