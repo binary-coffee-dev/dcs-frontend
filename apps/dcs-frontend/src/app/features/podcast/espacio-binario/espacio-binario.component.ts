824bbdb1c4784081b4e163cce8bee2a30acb0a5b
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-08-16 15:13:29 -0400

    EspacioBinarioComponent base

--- apps/dcs-frontend/src/app/features/podcast/espacio-binario/espacio-binario.component.ts
+++ apps/dcs-frontend/src/app/features/podcast/espacio-binario/espacio-binario.component.ts
@@ -3,9 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-espacio-binario',
   templateUrl: './espacio-binario.component.html',
-  styleUrls: ['./espacio-binario.component.css']
+  styleUrls: ['./espacio-binario.component.scss']
 })
 export class EspacioBinarioComponent implements OnInit {
+
+  podcasts = [
+    { name: 'Episodio 02', duration: '10m', date: '1 day ago', imgUrl:'', link:'' },
+    { name: 'Episodio 01', duration: '17m', date: '5 days ago', imgUrl:'', link:'' },
+    { name: 'Episodio 00', duration: '13m', date: '10 days ago', imgUrl:'', link:'' }
+  ];
 
   constructor() { }
 
