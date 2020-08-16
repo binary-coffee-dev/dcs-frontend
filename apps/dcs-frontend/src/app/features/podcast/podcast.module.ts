3f242d7a398009889e0445857ee9319b8dea90b5
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-08-16 14:07:14 -0400

    PodcastModule import PodcastComponent and Routing

--- apps/dcs-frontend/src/app/features/podcast/podcast.module.ts
+++ apps/dcs-frontend/src/app/features/podcast/podcast.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { MaterialModule } from '@dcs-libs/shared';
+import { PodcastComponent } from './podcast.component';
+import { PodcastRoutingModule } from './podcast-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [PodcastComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    PodcastRoutingModule
   ]
 })
 export class PodcastModule { }
