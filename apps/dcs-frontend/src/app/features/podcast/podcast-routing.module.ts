2c17ea0ddcb89859941ac14867be4a373582707f
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-08-16 15:12:48 -0400

    Added espacio-binario route

--- apps/dcs-frontend/src/app/features/podcast/podcast-routing.module.ts
+++ apps/dcs-frontend/src/app/features/podcast/podcast-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PodcastComponent } from './podcast.component';
+import { EspacioBinarioComponent } from './espacio-binario/espacio-binario.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: PodcastComponent
+  },
+  {
+    path: 'espacio-binario',
+    component: EspacioBinarioComponent
   }
 ];
 
