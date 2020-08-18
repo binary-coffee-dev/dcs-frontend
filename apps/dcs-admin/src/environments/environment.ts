16afca4677973a058a9d0bfb495e8c4a87bb37c3
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-08-18 11:11:41 +0200

    Add podcastApiUtl env variable

--- apps/dcs-admin/src/environments/environment.ts
+++ apps/dcs-admin/src/environments/environment.ts
@@ -3,6 +3,7 @@ import { Environment } from '@dcs-libs/shared';
 export const environment = {
   apiUrl: 'http://localhost:1337/',
   graphqlUrl: 'http://localhost:1337/graphql',
+  podcastApiUrl: 'http://localhost:1337/graphql',
   siteUrl: 'http://localhost:4201',
   siteDashboardUrl: 'http://localhost:4201',
   production: false,
