d0e61dbe6ca80e3fc5f113ca5b513426d872bde1
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-08-18 19:49:55 +0200

    Integrate podcast list with recent-podcasts component (#334)

--- apps/dcs-frontend/src/app/features/dashboard/recent-podcasts/recent-podcasts.component.ts
+++ apps/dcs-frontend/src/app/features/dashboard/recent-podcasts/recent-podcasts.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Store } from '@ngxs/store';
+import { FetchPodcastAction, Podcast, PodcastState } from '@dcs-libs/shared';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { MomentService } from '../../../core/services';
 
 @Component({
   selector: 'app-recent-podcasts',
   templateUrl: './recent-podcasts.component.html',
   styleUrls: ['./recent-podcasts.component.scss']
 })
-export class RecentPodcastsComponent implements OnInit {
+export class RecentPodcastsComponent implements OnInit, OnDestroy {
 
-  podcasts = [
-    { name: 'Episodio 02', duration: '10m', date: '1 day ago', imgUrl:'', link:'' },
-    { name: 'Episodio 01', duration: '17m', date: '5 days ago', imgUrl:'', link:'' },
-    { name: 'Episodio 00', duration: '13m', date: '10 days ago', imgUrl:'', link:'' }
-  ];
+  _unsubscribe = new Subject();
 
-  constructor() { }
+  podcasts: Podcast[] = [];
 
-  ngOnInit(): void {
+  constructor(
+    private store: Store,
+    public moment: MomentService,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.store.dispatch(new FetchPodcastAction());
+    this.store.select(PodcastState.podcastList)
+      .pipe(takeUntil(this._unsubscribe))
+      .subscribe(list => this.podcasts = list || []);
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
+  }
+
+  toDate(podcast: Podcast): Date {
+    return podcast && podcast.date && new Date(podcast.date) || null;
+  }
 }
