9c859f9c94f513673da5c5b4df66c07da8453748
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-08-20 10:36:13 +0200

    Fix broken tests (#334)

--- apps/dcs-frontend/src/app/features/podcast/espacio-binario/espacio-binario.component.spec.ts
+++ apps/dcs-frontend/src/app/features/podcast/espacio-binario/espacio-binario.component.spec.ts
@@ -2,6 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { EspacioBinarioComponent } from './espacio-binario.component';
+import { Store } from '@ngxs/store';
+import { MomentService } from '../../../core/services';
+
+class StoreStub {
+}
+
+class MomentServiceStub {
+}
 
 describe('EspacioBinarioComponent', () => {
   let component: EspacioBinarioComponent;
@@ -9,15 +17,22 @@ describe('EspacioBinarioComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EspacioBinarioComponent ],
+      declarations: [EspacioBinarioComponent],
+      providers: [
+        {provide: Store, useClass: StoreStub},
+        {provide: MomentService, useClass: MomentServiceStub}
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EspacioBinarioComponent);
     component = fixture.componentInstance;
+
+    spyOn(component, 'ngOnInit');
+
     fixture.detectChanges();
   });
 
