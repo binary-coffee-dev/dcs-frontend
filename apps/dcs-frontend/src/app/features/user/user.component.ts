f66c1ea37b5259175a7c7ce83b1bd529a23f7ea4
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-08-24 02:15:01 -0400

    User component base struct

--- apps/dcs-frontend/src/app/features/user/user.component.ts
+++ apps/dcs-frontend/src/app/features/user/user.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+
+  top5Post = [
+    { name: 'binary-coffee', posts: 50 },
+    { name: 'binary-coffee2', posts: 35 },
+    { name: 'binary-coffee5', posts: 20 },
+    { name: 'binary-coffee6', posts: 15 },
+    { name: 'binary-coffee8', posts: 5 },
+  ]
+
+  top5Likes = [
+    { name: 'binary-coffee4', posts: 500 },
+    { name: 'binary-coffee8', posts: 350 },
+    { name: 'binary-coffee', posts: 200 },
+    { name: 'binary-coffee6', posts: 150 },
+    { name: 'binary-coffee2', posts: 50 },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  filterUser() {
+
+  }
+
+  getUserAvatar() {
+    return 'assets/images/noavatar.png'
   }
 
 }
