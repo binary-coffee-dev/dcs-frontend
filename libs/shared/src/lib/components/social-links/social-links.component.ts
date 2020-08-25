145c04e727147ac929f2dcfabe3ee48a9eb05ada
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-08-25 18:15:53 +0200

    Integrate legal components into the footer

--- libs/shared/src/lib/components/social-links/social-links.component.ts
+++ libs/shared/src/lib/components/social-links/social-links.component.ts
@@ -1,65 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { SocialLink } from './social-link.model';
+import { links } from './links-data';
 
 @Component({
   selector: 'app-social-links',
   templateUrl: './social-links.component.html',
   styleUrls: ['./social-links.component.scss']
 })
-export class SocialLinksComponent implements OnInit {
+export class SocialLinksComponent {
 
   @Input()
   footer = false;
 
-  links: SocialLink[];
+  links: SocialLink[] = links;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.links = [
-      {
-        enabled: true,
-        title: 'Facebook Page',
-        href: 'https://facebook.com/Binary-Coffee-111577483710684/',
-        icon: 'fa fa-facebook-official'
-      },
-      {
-        enabled: true,
-        title: 'GitHub Community',
-        href: 'https://github.com/dcs-community/',
-        icon: 'fa fa-github'
-      },
-      {
-        enabled: true,
-        title: 'Twitter Page',
-        href: 'https://twitter.com/CoffeeBinary/',
-        icon: 'fa fa-twitter'
-      },
-      {
-        enabled: false,
-        title: 'LinkedIn Page',
-        href: 'https://www.linkedin.com/company/binarycoffee/',
-        icon: 'fa fa-linkedin'
-      },
-      {
-        enabled: false,
-        title: 'Instagram Page',
-        href: 'https://www.instagram.com/binarycoffee.dev/',
-        icon: 'fa fa-instagram'
-      },
-      {
-        enabled: false,
-        title: 'BinaryCoffee Channel',
-        href: 'https://www.youtube.com/channel/UCmiGV-Amt7e4plFIxKH_v_g',
-        icon: 'fa fa-youtube-play'
-      },
-      {
-        enabled: true,
-        title: 'Telegram Channel',
-        href: 'https://t.me/binarycoffeedev',
-        icon: 'fa fa-telegram'
-      }
-    ];
+  constructor() {
   }
 }
--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { LoadingComponent } from './features/loading/loading.component';
 import { WINDOW, windowFactory } from './core/models';
@@ -9,7 +10,7 @@ import { CookiesConsentComponent } from './features/cookies-consent';
 import { MaterialModule } from './core/material';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   declarations: [
     LoadingComponent,
     HasPermissionsPipe,
