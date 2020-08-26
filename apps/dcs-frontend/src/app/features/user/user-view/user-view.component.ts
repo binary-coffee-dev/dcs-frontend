10683ab0973af2bc1b51bc3b8af35ca4ba9d1496
Karel Diaz Alfonso <kdiazalfonso94@gmail.com>
2020-08-26 12:46:04 -0400

    User view component base struct

--- apps/dcs-frontend/src/app/features/user/user-view/user-view.component.ts
+++ apps/dcs-frontend/src/app/features/user/user-view/user-view.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngxs/store';
+
+import {
+  UrlUtilsService,
+  MomentService,
+  Post,
+  PostState
+} from '@dcs-libs/shared';
+
 @Component({
   selector: 'app-user-view',
   templateUrl: './user-view.component.html',
-  styleUrls: ['./user-view.component.css']
+  styleUrls: ['./user-view.component.scss']
 })
 export class UserViewComponent implements OnInit {
 
-  constructor() { }
+  posts: Post[] = [];
 
-  ngOnInit(): void {
+  constructor(private store: Store, public moment: MomentService, public url: UrlUtilsService) { 
   }
 
+  ngOnInit(): void {
+    this.store.select(PostState.posts).subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
+  getUserAvatar() {
+    return 'assets/images/noavatar.png'
+  }
 }
