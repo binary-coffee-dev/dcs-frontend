3a276542254d9e93e07d11a090925ebf1bdd76cf
Guillermo Gonzalez <ggjnez92@gmail.com>
2021-04-08 22:36:47 +0200

    Integrate list of users in user-overview (#278)

--- apps/dcs-frontend/src/app/features/user/users-overview/users-overview.component.ts
+++ apps/dcs-frontend/src/app/features/user/users-overview/users-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
-import { FetchTopActiveUsersAction, FetchTopPopularUsersAction, TopUsers, UserInfoState } from '@dcs-libs/shared';
+import { FetchTopActiveUsersAction, FetchTopPopularUsersAction, FetchUsersAction, TopUsers, User, UserInfoState } from '@dcs-libs/shared';
 
 @Component({
   selector: 'app-user',
@@ -13,19 +13,23 @@ export class UsersOverviewComponent implements OnInit {
   top5Post = {} as TopUsers;
   top5Likes = {} as TopUsers;
 
+  users: User[] = [];
+
   constructor(private store: Store) {
   }
 
   ngOnInit(): void {
     this.store.select(UserInfoState.topPopularUsers).subscribe(topPopular => this.top5Likes = topPopular);
     this.store.select(UserInfoState.topActiveUsers).subscribe(topActive => this.top5Post = topActive);
+    this.store.select(UserInfoState.users).subscribe(users => this.users = users);
 
     this.store.dispatch(new FetchTopActiveUsersAction());
     this.store.dispatch(new FetchTopPopularUsersAction());
+    this.store.dispatch(new FetchUsersAction(''));
   }
 
-  filterUser() {
-
+  filterUser(event) {
+    this.store.dispatch(new FetchUsersAction(event.target.value));
   }
 
   getUserAvatar() {
