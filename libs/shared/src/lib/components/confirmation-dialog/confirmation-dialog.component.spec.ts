d2baad6a045388fbf42d496257634ca69435d57f
Guillermo Gonzalez <ggjnez92@gmail.com>
2021-04-27 22:43:45 +0200

    Fix broken tests (#308)

--- libs/shared/src/lib/components/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ libs/shared/src/lib/components/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+
+class MatDialogRefStub {
+}
 
 describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
@@ -8,9 +12,12 @@ describe('ConfirmationDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfirmationDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [ConfirmationDialogComponent],
+      providers: [
+        {provide: MatDialogRef, useClass: MatDialogRefStub},
+        {provide: MAT_DIALOG_DATA, useValue: {}}
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
