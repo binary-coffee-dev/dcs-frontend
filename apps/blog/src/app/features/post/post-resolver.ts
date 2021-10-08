b2c946497f81d3cda54f70afc5333c91fee83a00
Guillermo <ggjnez92@gmail.com>
2021-10-08 10:05:25 +0200

    Fix issue with build:ssr (#359)

--- apps/blog/src/app/features/post/post-resolver.ts
+++ apps/blog/src/app/features/post/post-resolver.ts
@@ -8,10 +8,11 @@ import { FetchPostAction, Post, PostState } from '@dcs-libs/shared';
 import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
-export class PostResolver implements Resolve<Post[]> {
-  constructor(private store: Store) {}
+export class PostResolver implements Resolve<Post> {
+  constructor(private store: Store) {
+  }
 
-  resolve(route: ActivatedRouteSnapshot): Observable<Post>|Promise<Post>|Post {
+  resolve(route: ActivatedRouteSnapshot): Observable<Post> | Promise<Post> | Post {
     return this.store.dispatch(new FetchPostAction(route.paramMap.get('id')))
       .pipe(map(() => {
         return this.store.selectSnapshot(PostState.post);
