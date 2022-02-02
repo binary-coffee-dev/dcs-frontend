e4404b14761d92ffa1103930324b0d900e9d556d
Guillermo <ggjnez92@gmail.com>
2022-02-02 01:52:02 +0100

    Update responsive in profile area and fix rss (#425)

--- libs/shared/src/lib/core/graphql/queries/users.ts
+++ libs/shared/src/lib/core/graphql/queries/users.ts
@@ -2,11 +2,13 @@ import { gql } from 'apollo-angular';
 
 export const GET_USERS = gql`
   query ($where: JSON!){
-    users(sort: "asd", limit: 10, start: 0, where: $where) {
+    users(sort: "asc", limit: 10, start: 0, where: $where) {
       id
       username
       avatarUrl
       name
+      page
+      email
     }
   }
 `;
