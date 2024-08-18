16b21c8c4e48e14217aa3f50ca41bef2885de6c1
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2024-08-18 17:07:36 +0200

    Fix issue with ssr configs (#488)

--- apps/blog/src/main.server.ts
+++ apps/blog/src/main.server.ts
@@ -1,15 +1,9 @@
 import { enableProdMode } from '@angular/core';
 
 import { environment } from './environments/environment';
-import { bootstrapApplication } from "@angular/platform-browser";
-import { AppServerModule } from "./app/app.server.module";
 
 if (environment.production) {
   enableProdMode();
 }
 
-const bootstrap = () => {
-  return bootstrapApplication(AppServerModule, {providers: []});
-};
-
-export default bootstrap;
+export {AppServerModule as default} from './app/app.server.module';
