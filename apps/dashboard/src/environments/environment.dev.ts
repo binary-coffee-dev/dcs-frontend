815c3c487b906492aea94552011461cfee2844dc
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2024-08-20 13:45:48 +0200

    Fix environment links

--- apps/dashboard/src/environments/environment.dev.ts
+++ apps/dashboard/src/environments/environment.dev.ts
@@ -1,20 +1,20 @@
 import { Environment } from '@dcs-libs/shared';
 
 export const environment = {
-  apiUrl: 'https://api-dev.binary-coffee.dev/',
-  graphqlUrl: 'https://api-dev.binary-coffee.dev/graphql',
-  podcastApiUrl: 'https://api-dev.binary-coffee.dev/podcasts/',
-  siteUrl: 'https://dev.binary-coffee.dev',
-  siteDashboardUrl: 'https://dev.binary-coffee.dev/dashboard',
+  apiUrl: 'https://api-dev.binarycoffee.dev/',
+  graphqlUrl: 'https://api-dev.binarycoffee.dev/graphql',
+  podcastApiUrl: 'https://api-dev.binarycoffee.dev/podcasts/',
+  siteUrl: 'https://dev.binarycoffee.dev',
+  siteDashboardUrl: 'https://dev.binarycoffee.dev/dashboard',
   production: false,
   local: false,
   githubClientId: '3fdd7684a78a0896c870',
   isDashboard: true,
   socials: {
     facebook: 'https://facebook.com/Binary-Coffee-111577483710684/',
-    github: 'https://github.com/binary-coffee-dev',
+    github: 'https://github.com/binarycoffee-dev',
     telegram: 'https://t.me/binarycoffeedev',
     twitter: 'https://twitter.com/CoffeeBinary/'
   },
-  contactMail: 'mailto:website@binary-coffee.dev'
+  contactMail: 'mailto:website@binarycoffee.dev'
 } as Environment;
