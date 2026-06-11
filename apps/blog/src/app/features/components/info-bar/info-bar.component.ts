d9cbc9059c584a18e15bbebcaaa429823f061d6e
guille <ggjnez92@gmail.com>
2026-06-11 20:11:05 +0200

    Migrate to zoneless info-bar component (#525)

--- apps/blog/src/app/features/components/info-bar/info-bar.component.ts
+++ apps/blog/src/app/features/components/info-bar/info-bar.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, OnDestroy, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+  inject,
+  input,
+  signal,
+  effect, computed,
+} from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 import { Store } from '@ngxs/store';
@@ -21,10 +31,10 @@ interface ShareLink {
 }
 
 @Component({
-    selector: 'app-info-bar',
-    templateUrl: './info-bar.component.html',
-    styleUrls: ['./info-bar.component.scss'],
-    standalone: false
+  selector: 'app-info-bar',
+  templateUrl: './info-bar.component.html',
+  styleUrls: ['./info-bar.component.scss'],
+  standalone: false,
 })
 export class InfoBarComponent implements OnInit, OnDestroy {
   private store = inject(Store);
@@ -32,38 +42,48 @@ export class InfoBarComponent implements OnInit, OnDestroy {
   url = inject(UrlUtilsService);
   private window = inject<Window>(WINDOW);
 
+  showPodcast = input<boolean>(true);
+  showRecentComments = input<boolean>(true);
+  showSocialMedias = input<boolean>(false);
 
-  @Input() showPodcast = true;
-  @Input() showRecentComments = true;
-  @Input() showSocialMedias = false;
+  episodes = signal<EpisodeModel[]>([]);
+  comments = signal<Comment[]>([]);
+  shareLinks = signal<ShareLink[]>([]);
+
+  lineNumbers = computed(() => {
+    let numberOfLines = 0;
+    if (this.showPodcast()) {
+      numberOfLines += this.episodes().length * 4 + 5;
+    }
+    if (this.showSocialMedias()) {
+      numberOfLines += this.shareLinks().length * 3 + 5;
+    }
+    if (this.showRecentComments()) {
+      numberOfLines += this.comments().length * 4 + 4;
+    }
+
+    return Object.keys(new Array(numberOfLines).fill(0)).map((i) => +i + 1);
+  });
 
   _unsubscribe = new Subject();
   isBrowser = true;
 
-  episodes: EpisodeModel[] = [];
-  comments: Comment[] = [];
-  shareLinks: ShareLink[] = [];
-
-  lineNumbers: number[] = [];
-
   constructor() {
-    const platformId = inject(PLATFORM_ID);
-
-    this.isBrowser = isPlatformBrowser(platformId);
+    this.isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
   }
 
   ngOnInit(): void {
-    this.store.select(PodcastState.episodesList)
+    this.store
+      .select(PodcastState.episodesList)
       .pipe(takeUntil(this._unsubscribe))
-      .subscribe(list => {
-        this.episodes = list || [];
-        this.calculateNumberOfLines();
+      .subscribe((list) => {
+        this.episodes.set(list || []);
       });
-    this.store.select(CommentState.recentComments)
+    this.store
+      .select(CommentState.recentComments)
       .pipe(takeUntil(this._unsubscribe))
-      .subscribe(comments => {
-        this.comments = comments || [];
-        this.calculateNumberOfLines();
+      .subscribe((comments) => {
+        this.comments.set(comments || []);
       });
     this.createShareLinks();
   }
@@ -71,32 +91,20 @@ export class InfoBarComponent implements OnInit, OnDestroy {
   createShareLinks() {
     if (this.isBrowser) {
       const currentUrl = this.window.document.location.href;
-      this.shareLinks = [
-        { name: 'Facebook', url: `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}` } as ShareLink,
+      this.shareLinks.set([
+        {
+          name: 'Facebook',
+          url: `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`,
+        } as ShareLink,
         {
           name: 'Twitter',
-          url: `https://twitter.com/intent/tweet/?hashtags=BinaryCoffee&url=${currentUrl}`
+          url: `https://twitter.com/intent/tweet/?hashtags=BinaryCoffee&url=${currentUrl}`,
         } as ShareLink,
-        { name: 'Linkedin', url: `https://www.linkedin.com/shareArticle?mini=true&url=${currentUrl}` } as ShareLink
-      ];
-      this.calculateNumberOfLines();
-    }
-  }
-
-  calculateNumberOfLines() {
-    let numberOfLines = 0;
-    if (this.showPodcast) {
-      numberOfLines += this.episodes.length * 4 + 5;
-    }
-    if (this.showSocialMedias) {
-      numberOfLines += this.shareLinks.length * 3 + 5;
-    }
-    if (this.showRecentComments) {
-      numberOfLines += this.comments.length * 4 + 4;
-    }
-    this.lineNumbers = [];
-    for (let i = 1; i <= numberOfLines; i++) {
-      this.lineNumbers.push(i);
+        {
+          name: 'Linkedin',
+          url: `https://www.linkedin.com/shareArticle?mini=true&url=${currentUrl}`,
+        } as ShareLink,
+      ]);
     }
   }
 
@@ -105,7 +113,7 @@ export class InfoBarComponent implements OnInit, OnDestroy {
   }
 
   toDate(date: string | undefined): Date | undefined {
-    return date && new Date(date) || undefined;
+    return (date && new Date(date)) || undefined;
   }
 
   getPostName(post: Post | string | undefined) {
