457d627b99bb19f44ab333b460a4f663aac712c7
guille <ggjnez92@gmail.com>
2026-06-12 19:57:12 +0200

    Migrate to zone-less scroll-top component (#525)

--- apps/blog/src/app/features/components/scroll-top/scroll-top.component.ts
+++ apps/blog/src/app/features/components/scroll-top/scroll-top.component.ts
@@ -1,17 +1,17 @@
-import { Component, HostListener, inject } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 
 import { ScrollService } from '../../../core/services';
 
 @Component({
-    selector: 'app-scroll-top',
-    templateUrl: './scroll-top.component.html',
-    styleUrls: ['./scroll-top.component.scss'],
-    standalone: false
+  selector: 'app-scroll-top',
+  templateUrl: './scroll-top.component.html',
+  styleUrls: ['./scroll-top.component.scss'],
+  standalone: false,
 })
 export class ScrollTopComponent {
   private scroll = inject(ScrollService);
 
-  windowScrolled = false;
+  windowScrolled = signal<boolean>(false);
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
@@ -20,13 +20,13 @@ export class ScrollTopComponent {
       document.documentElement.scrollTop ||
       document.body.scrollTop > 100
     ) {
-      this.windowScrolled = true;
+      this.windowScrolled.set(true);
     } else if (
-      (this.windowScrolled && window.pageYOffset) ||
+      (this.windowScrolled() && window.pageYOffset) ||
       document.documentElement.scrollTop ||
       document.body.scrollTop < 10
     ) {
-      this.windowScrolled = false;
+      this.windowScrolled.set(false);
     }
   }
 
