12bff82bcc7aa45bf285565c34273b515a3eae62
guille <ggjnez92@gmail.com>
2026-06-13 02:53:20 +0200

    Migrate to zone-less top-popular-users component (#525)

--- apps/blog/src/app/features/components/top-popular-users/top-popular-users.component.ts
+++ apps/blog/src/app/features/components/top-popular-users/top-popular-users.component.ts
@@ -1,31 +1,46 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 
 import { Store } from '@ngxs/store';
 
-import { TopUsers, UrlUtilsService, User, UserInfoState } from '@dcs-libs/shared';
+import {
+  TopUsers,
+  UrlUtilsService,
+  User,
+  UserInfoState,
+} from '@dcs-libs/shared';
 
 @Component({
-    selector: 'app-top-popular-users',
-    templateUrl: './top-popular-users.component.html',
-    styleUrls: ['./top-popular-users.component.scss'],
-    standalone: false
+  selector: 'app-top-popular-users',
+  templateUrl: './top-popular-users.component.html',
+  styleUrls: ['./top-popular-users.component.scss'],
+  standalone: false,
 })
 export class TopPopularUsersComponent implements OnInit {
   private store = inject(Store);
   url = inject(UrlUtilsService);
 
-  top5Likes = {} as unknown as TopUsers;
+  top5Likes = signal<TopUsers | null>(null);
 
   ngOnInit(): void {
-    this.store.select(UserInfoState.topPopularUsers).subscribe(topPopular => this.top5Likes = {...topPopular});
+    this.store
+      .select(UserInfoState.topPopularUsers)
+      .subscribe((topPopular) => this.top5Likes.set({ ...topPopular }));
   }
 
   onUserImgError(index: number): void {
-    this.top5Likes.users = this.top5Likes.users.map((user: User, i: number): User => {
-      if (i == index) {
-        return {...user, avatarUrl: undefined} as User;
+    this.top5Likes.update((top5Likes: TopUsers | null): TopUsers | null => {
+      if (top5Likes) {
+        return {
+          ...top5Likes,
+          users: top5Likes?.users?.map((user: User, i: number): User => {
+            if (i == index) {
+              return { ...user, avatarUrl: undefined } as User;
+            }
+            return user;
+          }),
+        } as TopUsers;
       }
-      return user;
+      return top5Likes;
     });
   }
 
@@ -34,9 +49,6 @@ export class TopPopularUsersComponent implements OnInit {
   }
 
   getTopLikeByIndex(i: number): string | number {
-    if (this.top5Likes && this.top5Likes.values) {
-      return this.top5Likes.values[i];
-    }
-    return '';
+    return this.top5Likes()?.values?.[i] ?? '';
   }
 }
