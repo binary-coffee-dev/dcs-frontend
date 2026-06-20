b5cf233a5d46273f8cdd265a53734c5b4d1b870a
guille <ggjnez92@gmail.com>
2026-06-20 15:38:23 +0200

    Fix lint issue and reformat code (#525)

--- apps/blog/src/app/features/app.component.ts
+++ apps/blog/src/app/features/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, inject, computed } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { isPlatformBrowser } from '@angular/common';
@@ -17,6 +17,7 @@ import {
   SubscribeDialogComponent
 } from '@dcs-libs/shared';
 import { consoleMessage } from './console.log';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 declare let gtag: (property: string, value: string, configs: object) => void;
 declare let moment: any;
@@ -40,6 +41,24 @@ export class AppComponent implements OnInit {
   private environment = inject<Environment>(ENVIRONMENT);
   private platformId = inject(PLATFORM_ID);
 
+  isSessionOpen = toSignal<boolean>(this.store.select(AuthState.isLogin), { initialValue: false });
+  lastOpenDate = toSignal(
+    this.store.selectSnapshot(ConfigState.getConfigItem(SUBSCRIPTION_WAS_OPENED_CONFIG_KEY)),
+    { initialValue: Date.now() }
+  );
+
+  dialogWasShown = computed(() => {
+    try {
+      const date = new Date(this.lastOpenDate());
+      return date.getDate() === new Date().getDate();
+    } catch (er) {
+      console.error(er);
+    }
+    return false;
+  });
+
+  canOpenSubscription = computed<boolean>(() => !this.isSessionOpen() && !this.dialogWasShown());
+
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       moment.locale('es');
@@ -56,8 +75,7 @@ export class AppComponent implements OnInit {
       // eslint-disable-next-line no-console
       console.log(consoleMessage);
 
-      // toDo 27.01.22, guille, show subscription in new version
-      // this.showSubscriptionDialog();
+      this.showSubscriptionDialog();
     }
     this.metaTags.addLinkTag(
       {
@@ -72,39 +90,15 @@ export class AppComponent implements OnInit {
 
   showSubscriptionDialog() {
     if (this.canOpenSubscription()) {
-      timer(TIME_TO_OPEN_SUBSCRIPTION_DIALOG_IN_MIL).subscribe(() => {
-        this.dialog
-          .open(SubscribeDialogComponent, {
-            disableClose: true
-          })
-          .afterClosed()
-          .subscribe(() => {
-            this.store.dispatch(
-              new SetConfigAction(SUBSCRIPTION_WAS_OPENED_CONFIG_KEY, new Date())
-            );
-          });
-      });
+      timer(TIME_TO_OPEN_SUBSCRIPTION_DIALOG_IN_MIL)
+        .pipe(
+          mergeMap(() =>
+            this.dialog.open(SubscribeDialogComponent, { disableClose: true }).afterClosed()
+          )
+        )
+        .subscribe(() => {
+          this.store.dispatch(new SetConfigAction(SUBSCRIPTION_WAS_OPENED_CONFIG_KEY, new Date()));
+        });
     }
-  }
-
-  canOpenSubscription(): boolean {
-    return !this.isSessionOpen() && !this.dialogWasShown();
-  }
-
-  isSessionOpen(): boolean {
-    return this.store.selectSnapshot(AuthState.isLogin);
-  }
-
-  dialogWasShown(): boolean {
-    const lastOpenDate = this.store.selectSnapshot(
-      ConfigState.getConfigItem(SUBSCRIPTION_WAS_OPENED_CONFIG_KEY)
-    );
-    try {
-      const date = new Date(lastOpenDate);
-      return date.getDate() === new Date().getDate();
-    } catch (er) {
-      console.error(er);
-    }
-    return false;
   }
 }
